eb2ff16f3c63d8dc96b738edbfed5b42d51df9e9
Vlad1kun <viparrow@mail.ru>
2023-03-15 22:26:49 +0300

    work: get_in_groups

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 // Аунтіфікація    https://oauth.vk.com/authorize?client_id=51581045&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=groups,friends&response_type=token&v=5.131&state=123456
 // апи токен  https://oauth.vk.com/blank.html#access_token=vk1.a.1ouR08E0DRBClj4k2SHbnN_RYKvvQBZku__x2luF3YDWIXfjMNb8uaF1pCpYCDWrnufeCeogOu1sMzYST8myi7hefCdN9O-Ym142c8n8DC8oo7mD0Z8Go3wjOKWnjpljf52Linpr0KyvZD2fRsNOVz2VYcVlfq2TkdOTmsNEKcq-8aUp9ZyWJN-Etn-81j4A0ylTfFryhE1vDS__VMf2Bg&expires_in=86400&user_id=185702954&state=123456
 // token vk1.a.1ouR08E0DRBClj4k2SHbnN_RYKvvQBZku__x2luF3YDWIXfjMNb8uaF1pCpYCDWrnufeCeogOu1sMzYST8myi7hefCdN9O-Ym142c8n8DC8oo7mD0Z8Go3wjOKWnjpljf52Linpr0KyvZD2fRsNOVz2VYcVlfq2TkdOTmsNEKcq-8aUp9ZyWJN-Etn-81j4A0ylTfFryhE1vDS__VMf2Bg
+
+// https://oauth.vk.com/authorize?client_id=51581045&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=groups,friends&response_type=token&v=5.131&state=123456
+// #access_token=vk1.a.WKA9ehYIIozMAbJbPLj-lzx7_jSBT4fia18I3y2--MOGwtoARyMsQJ0V7P-NLsVEZJi4p0Ca265fH-kDIgF0ANnueNpa_cDWck5XI_PeuUako-EGtcwL4JdBKOuhFEO0EEj_pcF6m-RivmzbFABqNMb8NGH8UfQ79X1wexwpNuokvy_JoCl0s8FUF57XI8A2bWXC97C66vi8d4XDKCwtQg
