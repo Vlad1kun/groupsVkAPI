6edf72136a6b78297e6cef5b4b0cdb874d7ca4ab
Vlad1kun <viparrow@mail.ru>
2023-03-16 23:18:58 +0300

    work: api_WORK

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 // token vk1.a.1ouR08E0DRBClj4k2SHbnN_RYKvvQBZku__x2luF3YDWIXfjMNb8uaF1pCpYCDWrnufeCeogOu1sMzYST8myi7hefCdN9O-Ym142c8n8DC8oo7mD0Z8Go3wjOKWnjpljf52Linpr0KyvZD2fRsNOVz2VYcVlfq2TkdOTmsNEKcq-8aUp9ZyWJN-Etn-81j4A0ylTfFryhE1vDS__VMf2Bg
 
 // https://oauth.vk.com/authorize?client_id=51581045&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=groups,friends&response_type=token&v=5.131&state=123456
-// #access_token=vk1.a.WKA9ehYIIozMAbJbPLj-lzx7_jSBT4fia18I3y2--MOGwtoARyMsQJ0V7P-NLsVEZJi4p0Ca265fH-kDIgF0ANnueNpa_cDWck5XI_PeuUako-EGtcwL4JdBKOuhFEO0EEj_pcF6m-RivmzbFABqNMb8NGH8UfQ79X1wexwpNuokvy_JoCl0s8FUF57XI8A2bWXC97C66vi8d4XDKCwtQg
+// access_token= vk1.a.1JOBAiR6Nar8ZRQ9kUQu-siwAxvW4DhZSeXgWVmjQo3cTHIJFRZ8WYdCEn7kDZMu-TNzAQdAfQN8SAdA2onQOAiC2dwtNj-3w7K4mBSWpOqJx823qQ9OOcX5tg9L4MbfIoOvK51KJMEBembRVlh8MWUuOym6GsaPfSwt8-o_Bf8gC9-NLAksprIdOmTUK2XKw-qLD6u-24_sN4ve8t-b0A
